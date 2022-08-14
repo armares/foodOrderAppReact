3954b8e8aff5ac2306dae36e0e725c650a19a0be
armares <armares2001@libero.it>
2022-08-14 15:50:30 +0200

    project static + modal Order

---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+const Input = ({label,input}) => {
+  return (
+    <div className={styles.input}>
+        <label htmlFor={input.id}>{label}</label>
+        <input id={input.id} {...input}/>
+    </div>
+  )
+}
+
+export default Input
